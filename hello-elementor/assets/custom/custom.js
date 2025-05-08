3114e4431852eb04304edaadd95b1d1d1f258f7e
Wp_devsite <leshiy_pos@mail.ru>
2025-05-08 21:58:39 +0300

    Resolve tasks 9, 10

--- hello-elementor/assets/custom/custom.js
+++ hello-elementor/assets/custom/custom.js
@@ -11,8 +11,22 @@ window.addEventListener("load", () => {
     if (metaTag[1]) {
       metaTag[1].remove();
     } else {
-      console.log("Метотег не найден");
+      //   console.log("Метотег не найден");
     }
   }
   deleteMetaTag();
+
+  //   Filling empty ALT attributes with text from H1 headings
+
+  function setAtributsImg() {
+    let contentH1 = document.querySelector("h1").textContent;
+    let imgesTag = document.querySelectorAll("img");
+    imgesTag.forEach((img) => {
+      if (!img.hasAttribute("alt") || img.getAttribute("alt") === "") {
+        img.setAttribute("alt", contentH1);
+      }
+    });
+  }
+
+  setTimeout(setAtributsImg, 1000);
 });
