e553b7d050f9314636d1d678d43e90ab76c87d6f
Wp_devsite <leshiy_pos@mail.ru>
2025-07-14 09:00:21 +0300

    added shopping cart icon in header

--- upakovych_elementor/functions.php
+++ upakovych_elementor/functions.php
@@ -497,6 +497,7 @@ function discount_percentage_func(){
 add_shortcode('mobile_menu_custom', 'mobile_menu_function');
 
 function mobile_menu_function(){
+	$count = WC()->cart->get_cart_contents_count();
 	?>
 	<div class="header_mobile">
 		<div class="wrap">
@@ -542,7 +543,12 @@ function mobile_menu_function(){
 		<li><a class="" href="https://wa.me/79921854801" target="_blank"><span class="elementor-screen-only">Whatsapp</span><svg class="e-font-icon-svg e-fab-whatsapp" viewBox="0 0 448 512" xmlns="http://www.w3.org/2000/svg"><path d="M380.9 97.1C339 55.1 283.2 32 223.9 32c-122.4 0-222 99.6-222 222 0 39.1 10.2 77.3 29.6 111L0 480l117.7-30.9c32.4 17.7 68.9 27 106.1 27h.1c122.3 0 224.1-99.6 224.1-222 0-59.3-25.2-115-67.1-157zm-157 341.6c-33.2 0-65.7-8.9-94-25.7l-6.7-4-69.8 18.3L72 359.2l-4.4-7c-18.5-29.4-28.2-63.3-28.2-98.2 0-101.7 82.8-184.5 184.6-184.5 49.3 0 95.6 19.2 130.4 54.1 34.8 34.9 56.2 81.2 56.1 130.5 0 101.8-84.9 184.6-186.6 184.6zm101.2-138.2c-5.5-2.8-32.8-16.2-37.9-18-5.1-1.9-8.8-2.8-12.5 2.8-3.7 5.6-14.3 18-17.6 21.8-3.2 3.7-6.5 4.2-12 1.4-32.6-16.3-54-29.1-75.5-66-5.7-9.8 5.7-9.1 16.3-30.3 1.8-3.7.9-6.9-.5-9.7-1.4-2.8-12.5-30.1-17.1-41.2-4.5-10.8-9.1-9.3-12.5-9.5-3.2-.2-6.9-.2-10.6-.2-3.7 0-9.7 1.4-14.8 6.9-5.1 5.6-19.4 19-19.4 46.3 0 27.3 19.9 53.7 22.6 57.4 2.8 3.7 39.1 59.7 94.8 83.8 35.2 15.2 49 16.5 66.6 13.9 10.7-1.6 32.8-13.4 37.4-26.4 4.6-13 4.6-24.1 3.2-26.4-1.3-2.5-5-3.9-10.5-6.6z"></path></svg>					</a></li>
 		<li><a class="" href="mailto:7795@upakovych.ru" target="_blank"><span class="elementor-screen-only">Icon-email</span><svg xmlns="http://www.w3.org/2000/svg" id="Layer_1" data-name="Layer 1" viewBox="0 0 24 24"><path d="M19.5,2H4.5C2.019,2,0,4.019,0,6.5v11c0,2.481,2.019,4.5,4.5,4.5h15c2.481,0,4.5-2.019,4.5-4.5V6.5c0-2.481-2.019-4.5-4.5-4.5ZM4.5,3h15c1.084,0,2.043,.506,2.686,1.283l-7.691,7.692c-.662,.661-1.557,1.025-2.497,1.025-.914-.017-1.826-.36-2.492-1.025L1.814,4.283c.643-.777,1.601-1.283,2.686-1.283Zm18.5,14.5c0,1.93-1.57,3.5-3.5,3.5H4.5c-1.93,0-3.5-1.57-3.5-3.5V6.5c0-.477,.097-.931,.271-1.346l7.528,7.528c.851,.851,1.98,1.318,3.177,1.318s2.375-.467,3.226-1.318l7.528-7.528c.174,.415,.271,.869,.271,1.346v11Z"/></svg></a></li>
 		<li><a class="" href="#request_call"><span class="elementor-screen-only">Icon-phone-call</span><svg xmlns="http://www.w3.org/2000/svg" id="Layer_1" data-name="Layer 1" viewBox="0 0 24 24" width="512" height="512"><path d="M13,1a1,1,0,0,1,1-1A10.011,10.011,0,0,1,24,10a1,1,0,0,1-2,0,8.009,8.009,0,0,0-8-8A1,1,0,0,1,13,1Zm1,5a4,4,0,0,1,4,4,1,1,0,0,0,2,0,6.006,6.006,0,0,0-6-6,1,1,0,0,0,0,2Zm9.093,10.739a3.1,3.1,0,0,1,0,4.378l-.91,1.049c-8.19,7.841-28.12-12.084-20.4-20.3l1.15-1A3.081,3.081,0,0,1,7.26.906c.031.031,1.884,2.438,1.884,2.438a3.1,3.1,0,0,1-.007,4.282L7.979,9.082a12.781,12.781,0,0,0,6.931,6.945l1.465-1.165a3.1,3.1,0,0,1,4.281-.006S23.062,16.708,23.093,16.739Zm-1.376,1.454s-2.393-1.841-2.424-1.872a1.1,1.1,0,0,0-1.549,0c-.027.028-2.044,1.635-2.044,1.635a1,1,0,0,1-.979.152A15.009,15.009,0,0,1,5.9,9.3a1,1,0,0,1,.145-1S7.652,6.282,7.679,6.256a1.1,1.1,0,0,0,0-1.549c-.031-.03-1.872-2.425-1.872-2.425a1.1,1.1,0,0,0-1.51.039l-1.15,1C-2.495,10.105,14.776,26.418,20.721,20.8l.911-1.05A1.121,1.121,0,0,0,21.717,18.193Z"/></svg></a></li>
-		<li><a class="" href="https://upakovych.ru/cart/" target="_blank"><span class="elementor-screen-only">Shopping-basket</span><svg class="e-font-icon-svg e-fas-shopping-basket" viewBox="0 0 576 512" xmlns="http://www.w3.org/2000/svg"><path d="M576 216v16c0 13.255-10.745 24-24 24h-8l-26.113 182.788C514.509 462.435 494.257 480 470.37 480H105.63c-23.887 0-44.139-17.565-47.518-41.212L32 256h-8c-13.255 0-24-10.745-24-24v-16c0-13.255 10.745-24 24-24h67.341l106.78-146.821c10.395-14.292 30.407-17.453 44.701-7.058 14.293 10.395 17.453 30.408 7.058 44.701L170.477 192h235.046L326.12 82.821c-10.395-14.292-7.234-34.306 7.059-44.701 14.291-10.395 34.306-7.235 44.701 7.058L484.659 192H552c13.255 0 24 10.745 24 24zM312 392V280c0-13.255-10.745-24-24-24s-24 10.745-24 24v112c0 13.255 10.745 24 24 24s24-10.745 24-24zm112 0V280c0-13.255-10.745-24-24-24s-24 10.745-24 24v112c0 13.255 10.745 24 24 24s24-10.745 24-24zm-224 0V280c0-13.255-10.745-24-24-24s-24 10.745-24 24v112c0 13.255 10.745 24 24 24s24-10.745 24-24z"></path></svg>					</a></li>
+		<li>	
+			<a class="cart-quantity header-icon-cart" href="/cart/">
+				<img src="<?php echo get_template_directory_uri(); ?>/assets/images/shopping-cart.svg" alt="">
+				<span class="count<?php echo ($count === 0)?" no-items": ""; ?>"><?php echo !($count === 0)?$count: ""; ?></span>
+			</a>
+		</li>
 		</ul>
 	<!-- Конец Иконки -->
 	</div>
@@ -636,6 +642,29 @@ function mobile_menu_catalog_func(){
 	</div>
 	<?php
 }
+// Корзина
+add_shortcode('custom_shoppping_basket', 'custom_shoppping_basket_func');
+
+function custom_shoppping_basket_func(){
+	$count = WC()->cart->get_cart_contents_count();
+	?>
+	<a class="cart-quantity header-icon-cart" href="/cart/">
+		<img src="<?php echo get_template_directory_uri(); ?>/assets/images/shopping-cart.svg" alt="">
+    	<span class="count<?php echo ($count === 0)?" no-items": ""; ?>"><?php echo !($count === 0)?$count: ""; ?></span>
+	</a>
+	<?php
+}
+add_filter( 'woocommerce_add_to_cart_fragments', 'custom_ajax_cart_count_fragment' );
+function custom_ajax_cart_count_fragment( $fragments ) {
+    ob_start();
+    ?>
+    <span class="count"><?php echo WC()->cart->get_cart_contents_count(); ?></span>
+    <?php
+    $fragments['.cart-quantity .count'] = ob_get_clean();
+    return $fragments;
+}
+
+
 // Добавляем облако тэгов
 
 add_action('woocommerce_before_shop_loop', 'upakovych_show_product_tags_cloud', 4);
@@ -713,7 +742,7 @@ add_action('woocommerce_before_shop_loop', function() {
 			<div class="filter_catalog_wrap">
 				<div class="header_filters not_active">
 					<h6>Фильтры</h6>
-					<svg width="8" height="14" viewBox="0 0 8 14" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M7 13L1 7L7 1" stroke="#000000" stroke-width="1" stroke-linecap="round" stroke-linejoin="round"></path></svg>
+					<svg width="8" height="14" viewBox="0 0 8 14" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M7 13L1 7L7 1" stroke="#fff" stroke-width="1" stroke-linecap="round" stroke-linejoin="round"></path></svg>
 				</div>
 				<div class="filters not_active">
 		<?php
